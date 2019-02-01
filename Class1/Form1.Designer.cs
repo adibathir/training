9079db77f267cb3a5e4be18bb7d5bf727527b569
Zulfakor <muhammad.athir.adib.zulfakor@intel.com>
2019-02-01 22:31:34 +0800

    Adding New Training
    Signed-off-by: Zulfakor <muhammad.athir.adib.zulfakor@intel.com>

---
+++ Class1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Class1
+{
+    partial class Form_Training
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Press = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_Press
+            // 
+            this.button_Press.Location = new System.Drawing.Point(325, 154);
+            this.button_Press.Name = "button_Press";
+            this.button_Press.Size = new System.Drawing.Size(138, 88);
+            this.button_Press.TabIndex = 0;
+            this.button_Press.Text = "Add";
+            this.button_Press.UseVisualStyleBackColor = true;
+            this.button_Press.Click += new System.EventHandler(this.button_Press_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(325, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(325, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(325, 100);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 3;
+            // 
+            // Form_Training
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 419);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_Press);
+            this.Name = "Form_Training";
+            this.Text = "Training";
+            this.Load += new System.EventHandler(this.Form_Training_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Press;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Class1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Class1
+{
+    public partial class Form_Training : Form
+    {
+        public Form_Training()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Training_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button_Press_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int A = Convert.ToInt16(textBox1.Text);
+                int B = Convert.ToInt16(textBox2.Text);
+                textBox3.Text = ((A / B) + (A % B) / 2.0).ToString();
+            }
+            catch(Exception x)
+            {
+                MessageBox.Show(x.ToString());
+
+            }
+        }
+    }
+}
