623bab52eef8c10b25e46d3820ec80a5333cf139
Zulfakor <muhammad.athir.adib.zulfakor@intel.com>
2019-02-01 23:20:14 +0800

    separation
    Signed-off-by: Zulfakor <muhammad.athir.adib.zulfakor@intel.com>

--- Class1/Form1.cs
+++ Class1/Form1.cs
@@ -12,6 +12,7 @@ namespace Class1
 {
     public partial class Form_Training : Form
     {
+        string input1 = "A1237B1227C";
         public Form_Training()
         {
             InitializeComponent();
@@ -19,22 +20,36 @@ namespace Class1
 
         private void Form_Training_Load(object sender, EventArgs e)
         {
-           
+
         }
 
         private void button_Press_Click(object sender, EventArgs e)
         {
-            try
-            {
-                int A = Convert.ToInt16(textBox1.Text);
-                int B = Convert.ToInt16(textBox2.Text);
-                textBox3.Text = ((A / Ba) + (A % B) / 2.0).ToString();
-            }
-            catch(Exception x)
-            {
-                MessageBox.Show(x.ToString());
+            int count = 1;
+            string inputA = string.Empty;
+            string inputB = string.Empty;
+            int ans=0;
+            int ans2=0;
+            string ans3 = string.Empty;
+            if (input1[0] == 'A')
+                while (input1[count] != 'B')
+                {
+                    inputA += input1[count];
+                    ans = Convert.ToInt16(inputA);
+                    count++;
+                }
 
-            }
+            count++;
+
+                while (input1[count] != 'C')
+                {
+                inputB += input1[count];
+                ans2 = Convert.ToInt16(inputB);
+                count++;
+                }
+
+            ans3 = (ans + ans2).ToString();
+            MessageBox.Show(ans3);
         }
     }
 }
